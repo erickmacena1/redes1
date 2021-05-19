eb729fa29243dd26bfa6eb517aa4de4bc0fe2ea0
Jordan <jordanantonio_2000@hotmail.com>
2021-05-19 14:30:35 -0300

    codigo do cliente de testes e servidor concluido

--- servidor.py
+++ servidor.py
@@ -1,76 +1,68 @@
-# import socket programming library
 import socket
-  
-# import thread module
+import pickle
+
 from _thread import *
 import threading
-  
+
+#variáveis globais usadas no programa, usadas para testar com o cliente  
 print_lock = threading.Lock()
-positions = ['1', '2', '3']
 scoreboard = []
 msg_01 = '#01'
 msg_02 = '#02'
 msg_05 = '#05'
 msg_err = 'Error'
   
-# função que adiciona ao scoreboard
+#função que adiciona ao scoreboard
 def add_to_scoreboard(c):
     while True:
   
-        # data received from client
+        # recebe os dados do cliente, caso não receba nada ele abre a conexão
         data = c.recv(1024)
         if not data:
             print('Conexão encerrada')
-              
-            # lock released on exit
             print_lock.release()
             break
   
-        # receber a tupla enviada pelo cliente
+        #recebe o score e o adiciona no array
         scoreboard.append(data.decode())
-        scoreboard.sort()
+        scoreboard.sort(reverse = True)
 
-    # connection closed
+    #fecha a conexão
     c.close()
 
 #função que envia o scoreboard
-def send_scoreboard(c):           
-
-    for x in positions:
-        c.send(x.encode())
-    for y in scoreboard:
-        c.send(y.encode())
+def send_scoreboard(c):
+    sent_sco = pickle.dumps(scoreboard)
+    c.send(sent_sco)
   
-    # connection closed
-    c.close()
-  
+#função intermediária que recebe os pedidos do cliente e aciona as funções certas
 def select_options(c):
     while True:
 
-        # recebe os dados do cliente, caso não receba nada, ele abre a conexão
+        # recebe os dados do cliente, caso não receba nada ele abre a conexão
         data = c.recv(1024)
         print(data.decode())
         if not data:
             print('Conexão encerrada')
-                
-            # lock released on exit
             print_lock.release()
             break
 
         #de acordo com a opção enviada pelo cliente, ele envia para uma outra função
         if data.decode() == '1':
-            print('Recieved message 1')
+            print('Recebido pedido 1')
             c.send(msg_01.encode())
             add_to_scoreboard(c)
 
         elif data.decode() == '2':
-            print('Recieved message 2')
+            print('Recebido pedido 2')
             c.send(msg_02.encode())
             send_scoreboard(c)
+            print_lock.release()
 
         else:
-            print('Recieved error')
+            print('Código não reconhecido, fechando conexão')
             c.send(msg_err.encode())
+            print_lock.release()
 
         break
     c.close()
@@ -89,7 +81,7 @@ def Main():
     s.listen(3)
     print("socket está ouvindo")
   
-    # loop infinito até o cliente sair
+    # loop infinito até fechar o servidor
     while True:
   
         # estabelece uma conexão com o cliente
@@ -102,7 +94,6 @@ def Main():
         # inicia uma nova thread
         start_new_thread(select_options, (c,))
     s.close()
-  
   
 if __name__ == '__main__':
     Main()
